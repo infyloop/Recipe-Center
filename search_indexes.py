2e9dc2190057751a418c2323cfaa675bbba0523a
Saket <pure.vintage.skt@gmail.com>
2011-07-19 15:06:01 +0530

    Search Files added

---
+++ search_indexes.py
@@ -0,0 +1,18 @@
+
+
+import datetime
+from haystack.indexes import *
+from haystack import site
+from recipe.models import RecipeDump
+
+class RecipeIndex(SearchIndex):
+  text = CharField(document=True, use_template=True)
+  category = CharField(model_attr='category')
+  name = CharField(model_attr='name')
+  ingredients = CharField(model_attr='ingredients')
+  slug = CharField(model_attr='slug')
+  
+  def index_queryset(self):
+    return RecipeIndex.objects.filter(pub_date_lte=datetime.datetime.now())
+
+site.register(RecipeDump, RecipeIndex)
